87eacc87d81dce9ea6f88dfa18baf1ccfae2771b
NataliiaKarpenko <106306905+NataliiaKarpenko@users.noreply.github.com>
2023-07-24 19:53:22 +0000

    Deploying to gh-pages from @ NataliiaKarpenko/so-yummy-project@686c3bf252f19814661869806ab5160b6a6ae2e3 🚀

--- static/js/236.a6fe2ebd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkso_yummy_project=self.webpackChunkso_yummy_project||[]).push([[236],{4236:function(e,u,c){c.r(u);var r=c(3329);u.default=function(){return(0,r.jsx)("div",{children:"add recipe"})}}}]);
-//# sourceMappingURL=236.a6fe2ebd.chunk.js.map
